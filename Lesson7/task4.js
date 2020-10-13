5786fc06ff3438b655b79f48b8133fa8ace322ac
Boris Cherepakha <bcherepakha@gmail.com>
2020-10-13 13:48:15 +0300

    Lesson 7

---
+++ Lesson7/task4.js
@@ -0,0 +1,26 @@
+/**
+ * Дан обьект с баллами за задание
+*/
+const grade = {
+    Anton: getRandomBall(0, 90),
+    Maksym: 90,
+    Vladyslav: getRandomBall(),
+    Oleksii: getRandomBall(40),
+    Vadim: getRandomBall(40, 70),
+    Maxim: getRandomBall(15, 80)
+};
+
+function getRandomBall(min = 0, max = 100) {
+    return Math.round( min + Math.random() * (max - min) );
+}
+
+console.log(grade);
+
+/** Требуется:
+ 1. Указать имя учащегося с максимальным количеством баллов
+ 2. Указать максимальный балл.
+ 3. Указать средний балл.
+ 4. Указать учащегося с баллом ближайшим к среднему. (дома)
+ 5. Перечислить учащихся с баллом ниже среднего.
+ 6. Перечислить учащихся занявших первые три места в порядке убывания рейтинга.
+*/
